e59631fe960e0dde2e86f8a422396d8dda2fb94f
dschmerlaib <46738253+dschmerlaib@users.noreply.github.com>
2024-07-30 14:48:15 +0200

    wip Stage 2

--- Battleship with Java/task/src/battleship/Main.java
+++ Battleship with Java/task/src/battleship/Main.java
@@ -1,30 +1,38 @@
 package battleship;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[][] gameField = CreateGameField();
-        PrintGameField(gameField);
+        GameField gameField = new GameField();
+        Ships ships = new Ships();
+        gameField.PrintGameField();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the coordinates of the ship:");
-        String input = scanner.nextLine();
 
-        Coordinates coordinates = new Coordinates(input);
+        for (int i = 0; i < ships.Fleet.size(); i++) {
+            Ship ship = ships.Fleet.get(i);
 
-        if (coordinates.haveErrors) {
-            System.out.println("Error!");
-        } else {
+            System.out.println("Enter the coordinates of the " + ship.getInfoText());
+            boolean repeat = true;
+            while (repeat) {
 
-            int length = GetLength(coordinates);
-            String parts = GetPartsAsString(coordinates);
-            System.out.println("Length: " + length);
-            System.out.println("Parts: " + parts);
+                String input = scanner.nextLine();
+                Coordinates coordinates = ship.setPosition(input);
+                if (ship.position.haveErrors) {
+                    System.out.println("Error!");
+
+                } else {
+                    gameField.AddShipPosition(coordinates);
+                    gameField.PrintGameField();
+                    repeat = false;
+                }
+            }
 
         }
+
     }
 
 
@@ -35,8 +43,8 @@ public class Main {
 
         if (coordinates.first.row == coordinates.second.row) {
 
-            int x0 = coordinates.first.column;// Character.getNumericValue(x[1]);
-            int y0 = coordinates.second.column; //Character.getNumericValue(y[1]);
+            int x0 = coordinates.first.column;
+            int y0 = coordinates.second.column;
 
             if (x0 < y0) {
                 for (int i = x0; i <= y0; i++) {
@@ -53,11 +61,6 @@ public class Main {
                 }
             }
 
-           /* for (int i = x0; i <= y0; i++) {
-                stringBuilder.append(x[0]);
-                stringBuilder.append(i);
-                stringBuilder.append(" ");
-            }*/
         } else if (coordinates.first.row > coordinates.second.row) {
             for (char i = coordinates.second.row; i <= coordinates.first.row; i++) {
                 stringBuilder.append(i);
@@ -90,16 +93,6 @@ public class Main {
 
     }
 
-    private static void PrintGameField(String[][] gameField) {
-        for (int i = 0; i < 11; i++) {
-
-            for (int j = 0; j < 11; j++) {
-
-                System.out.print(gameField[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
 
     private static String[][] CreateGameField() {
         String[][] gameField = new String[11][11];
@@ -122,6 +115,81 @@ public class Main {
         return gameField;
     }
 
+}
+
+class GameField {
+    public String[][] Value; // [row][col]
+
+    public GameField() {
+        Value = CreateField();
+    }
+
+    private String[][] CreateField() {
+        String[][] gameField = new String[11][11];
+        char initChar = 'A';
+        for (int i = 0; i < 11; i++) {
+            if (i > 0) {
+                gameField[i][0] = String.valueOf(initChar);
+                ++initChar;
+
+                gameField[0][i] = String.valueOf(i);
+
+            } else {
+                gameField[i][0] = " ";
+                gameField[0][i] = " ";
+            }
+            for (int j = 1; j < 11; j++) {
+                gameField[i][j] = "~";
+            }
+        }
+        return gameField;
+    }
+
+    public void PrintGameField() {
+        for (int i = 0; i < 11; i++) {
+
+            for (int j = 0; j < 11; j++) {
+
+                System.out.print(Value[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void AddShipPosition(Coordinates coordinates) {
+
+        for (int i = 0; i < this.Value.length; i++) {
+        }
+
+        // same row
+        if (coordinates.first.row == coordinates.second.row) {
+            int index = 0;
+            for (int i = 0; i < this.Value.length; i++) {
+
+                index = i;
+                if (this.Value[i][0].equals(String.valueOf(coordinates.first.row))) {
+                    break;
+                }
+            }
+            int start = coordinates.first.column <= coordinates.second.column ? coordinates.first.column : coordinates.second.column;
+            int end = coordinates.first.column <= coordinates.second.column ? coordinates.second.column : coordinates.first.column;
+            for (int i = start; i <= end; i++) {
+                this.Value[index][i] = "O";
+            }
+
+        } else if (coordinates.first.column == coordinates.second.column) {
+            // gleiche spalte
+
+            for (int i = 1; i < this.Value.length; i++) { // 1.Zeile nummerierung
+                //Hier Fehler
+                char counter = this.Value[i][0].charAt(0);
+                if (counter > coordinates.first.column && counter <= coordinates.second.row) { //<= >=
+                    this.Value[i][coordinates.first.column] = "O";
+
+                }
+            }
+        }
+    }
 }
 
 class Coordinate {
@@ -154,6 +222,65 @@ class Coordinates {
         haveErrors = first.row != second.row && first.column != second.column || first.column > upperColumnBound || second.column > upperColumnBound || first.column < lowerColumnBound || second.column < lowerColumnBound || first.row < lowerRowBound || second.row < lowerRowBound || first.row > upperRowBound || second.row > upperRowBound;
 
 
+    }
+}
+
+class Ships {
+
+    public ArrayList<Ship> Fleet = new ArrayList<Ship>();
+
+    Ships() {
+        Fleet.add(new Ship(0, "Aircraft Carrier", 5));
+        Fleet.add(new Ship(1, "Battleship", 4));
+        Fleet.add(new Ship(2, "Submarine", 3));
+        Fleet.add(new Ship(3, "Cruiser", 3));
+        Fleet.add(new Ship(4, "Destroyer", 2));
+    }
+
+}
+
+class Ship {
+    public String name;
+    public int length;
+    public int orderNumber;
+
+    public Coordinates position;
+
+    public Ship(int orderNumber, String name, int length) {
+        this.name = name;
+        this.length = length;
+        this.orderNumber = orderNumber;
+    }
+
+    public String getInfoText() {
+        return name + "(" + length + " cells)";
+    }
+
+    public Coordinates setPosition(String position) {
+        this.position = new Coordinates(position);
+        this.position.haveErrors = !shipFits() || !shipInLine();
+
+        return this.position;
+    }
+
+    private boolean shipFits() {
+        return this.length == GetLength();
+    }
+
+    private boolean shipInLine() {
+        return this.position.first.row == this.position.second.row || this.position.first.column == this.position.second.column;
+    }
+
+    public int GetLength() {
+
+
+        if (this.position.first.row == this.position.second.row) {
+            return Math.abs(this.position.first.column - this.position.second.column) + 1;
+        } else if (this.position.first.row > this.position.second.row) {
+            return Math.abs(this.position.first.row - this.position.second.row) + 1;
+        } else {
+            return Math.abs(this.position.second.row - this.position.first.row) + 1;
+        }
     }
 }
 
